9fa1b9e9d54edb652c57e8b9d6c19155b7ae6b9b
Kritarth Sharma <kritarthsharma222@gmail.com>
2021-03-19 17:07:25 +0530

    Changed cells

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 14;
-const cellsVertical = 10;
+const cellsHorizontal = 5;
+const cellsVertical = 5;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
